c752b43bb9a7e27bb95ee6b5134e8822ae432fac
Kunal Chakraborty <kunalchakr32@gmail.com>
2024-08-25 05:27:24 +0000

    config done

---
+++ basic_01/datatype_02.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer code
+
+
+// alert(3+3) // we are using node js nlt browser
+
+let name = "Kunal"
+let age = 18
+let isloggedIn = false
+
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//symbol => unique
+
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
