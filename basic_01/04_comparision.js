3c4ff3916dc6ccf0c45ac681e2e2b94b0323d258
Kunal Chakraborty <kunalchakr32@gmail.com>
2024-08-26 03:47:16 +0000

    comparision config done

---
+++ basic_01/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//the above examples all are bad practice
+
+//   console.log("2" == 2);
+//   console.log("2" === 2); 
+  // '===' is a operator thst actuallychecks the datatype.  
+
+  console.log(1 === true);//output= false,bcz both diff datatype.
+  
+
+  
