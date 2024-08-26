e1d9cbea58a9b0b6b709e8202db08f3e09789357
Kunal Chakraborty <kunalchakr32@gmail.com>
2024-08-26 04:02:28 +0000

    extra interview related

---
+++ basic_01/02_datatype_summery.js
@@ -0,0 +1,8 @@
+// **************primitive datatype*************
+// there are 7 type of primitive datatype ...string,Number,Boolean,null,undefined,symbol,BigInt
+//And basicully these type of data type is CALL BY VALUE.....
+//as we knoow 'call by value' uses a copy of a main data type and make changes and return the value.
+
+//  *********************Reference (Nn primitive)******************
+
+//3 types .... Array,Objects,Function
